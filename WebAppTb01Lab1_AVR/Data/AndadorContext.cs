9d027546fe925e6fb601be95ff4bfcb75b069f41
Valdoni <antoniovaldoni@gmail.com>
2018-04-09 21:33:08 -0400

    Adicionar arquivos de projeto.

---
+++ WebAppTb01Lab1_AVR/Data/AndadorContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAppTb01Lab1_AVR.Models
+{
+    public class AndadorContext : DbContext
+    {
+        public AndadorContext (DbContextOptions<AndadorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppTb01Lab1_AVR.Models.Automovel> Automovel { get; set; }
+    }
+}
---
+++ WebAppTb01Lab1_AVR/Models/Automovel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTb01Lab1_AVR.Models
+{
+    public class Automovel
+    {
+        //PK
+        public int AutomovelId { get; set; }
+        [Required(ErrorMessage = "Tipo é obrigatório!")]
+        public Tipo Tipo { get; set; }
+
+        [Required(ErrorMessage ="Descrição tem que ser informada!")]
+        [StringLength(300, ErrorMessage = "O maximo de caracteres é 300!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Marca tem que ser informada!")]
+        [StringLength(100, ErrorMessage ="O maximo de caracteres é 100")]
+        public string Marca { get; set; }
+
+        [Required(ErrorMessage = "Disponivel tem que ser informado!")]
+        public bool Disponivel { get; set; }
+
+        [Required(ErrorMessage = "DataCadastro tem que ser informado! ")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
